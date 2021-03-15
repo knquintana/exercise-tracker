9ef0eb3a8f9572624bc20f4cdd030655c60351b3
knquintana <kimberly.quintana@checkr.com>
2021-03-14 21:37:55 -0700

    Calculate calorie burn on exercise creation/update

--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -1,5 +1,13 @@
+const ActivityService = require('../services/activity.service');
+const UserService = require('../services/user.service');
+
 const router = require('express').Router();
 let Exercise = require('../models/exercise.model');
+
+const {
+  calculateTotalCalorieBurn,
+  calculateActiveCalorieBurn,
+} = require('../../src/helpers');
 
 router.route('/').get((req, res) => {
   Exercise.find()
@@ -7,22 +15,42 @@ router.route('/').get((req, res) => {
           .catch(err => res.status(400).json(`Error: ${err}`));
 });
 
-router.route('/').post((req, res) => {
+router.route('/').post(async (req, res) => {
   const username = req.body.username;
-  const description = req.body.description;
+  const activityName = req.body.activityName;
   const duration = Number(req.body.duration);
-  const date = Date(req.body.date);
+  const date = Date.parse(req.body.date);
+
+  let user;
+  let activity;
+  try {
+    user = await UserService.getUserByUsername(username);
+    activity = await ActivityService.getActivityByName(activityName);
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json(`Error: ${err}`);
+  }
+
+  const totalCalories = calculateTotalCalorieBurn(
+    user.birthday, duration, user.gender, user.height, activity.metabolicEquivalent, user.weight
+  );
+  const activeCalories = calculateActiveCalorieBurn(
+    user.birthday, duration, user.gender, user.height, totalCalories, user.weight
+  );
 
   const newExercise = new Exercise({
     username,
-    description,
+    activityName,
     duration,
-    date
+    date,
+    activeCalories,
+    totalCalories
   });
 
   newExercise.save()
-             .then(() => res.json('Exercise added!'))
+             .then(() => res.json('Exercise Added!'))
              .catch(err => res.status(400).json(`Error: ${err}`));
+
 });
 
 router.route('/:id').get((req, res) => {
@@ -37,13 +65,38 @@ router.route('/:id').delete((req, res) => {
           .catch(err => res.status(400).json(`Error: ${err}`));
 })
 
-router.route('/:id').post((req, res) => {
+router.route('/:id').post(async (req, res) => {
+  const username = req.body.username;
+  const activityName = req.body.activityName;
+  const duration = Number(req.body.duration);
+  const date = Date(req.body.date);
+
+  let user;
+  let activity;
+  try {
+    user = await UserService.getUserByUsername(username);
+    activity = await ActivityService.getActivityByName(activityName);
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json(`Error: ${err}`);
+  }
+
   Exercise.findById(req.params.id)
           .then(exercise => {
-            exercise.username = req.body.username;
-            exercise.description = req.body.description;
-            exercise.duration = Number(req.body.duration);
-            exercise.date = Date.parse(req.body.date);
+            exercise.username = username;
+            exercise.activityName = activityName;
+            exercise.duration = duration;
+            exercise.date = date;
+
+            const totalCalories = calculateTotalCalorieBurn(
+              user.birthday, duration, user.gender, user.height, activity.metabolicEquivalent, user.weight
+            );
+            const activeCalories = calculateActiveCalorieBurn(
+              user.birthday, duration, user.gender, user.height, totalCalories, user.weight
+            );
+
+            exercise.totalCalories = totalCalories;
+            exercise.activeCalories = activeCalories;
 
             exercise.save()
                     .then(() => res.json('Exercise updated!'))
